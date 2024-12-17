a2f1e78ab2f9d52324d665f6daa5fb31e2af76a8
Artyom Ulanchik <artyom.u@outlook.com>
2024-12-17 06:46:05 +0000

    sorting by title

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { RootState } from "@/store";
 import { deleteNote } from "@/store/reducers/notes";
 import { useSelector, useDispatch } from "react-redux";
@@ -9,6 +9,20 @@ const NoteList: FC<INoteList> = ({ onEdit, onEditCancel }): JSX.Element => {
   const notes = useSelector((state: RootState) => state.notes.notes);
   const dispatch = useDispatch();
 
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+  };
+
+  const sortedNotes =
+    notes.length > 1
+      ? notes.slice().sort((a, b) => {
+          if (sortOrder === "asc") return a.title.localeCompare(b.title);
+          return b.title.localeCompare(a.title);
+        })
+      : notes;
+
   const handleDelete = (id: string) => {
     dispatch(deleteNote(id));
     onEditCancel();
@@ -16,7 +30,12 @@ const NoteList: FC<INoteList> = ({ onEdit, onEditCancel }): JSX.Element => {
 
   return (
     <div className="space-y-4">
-      {notes.map((note) => (
+      <div className="flex justify-end mb-4">
+        <button onClick={toggleSortOrder}>
+          Sort: {sortOrder === "asc" ? "Ascending" : "Descending"}
+        </button>
+      </div>
+      {sortedNotes.map((note) => (
         <NoteListItem
           key={note.id}
           note={note}
